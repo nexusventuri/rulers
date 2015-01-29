a750710c990f56e0a818b4a1d338829fddae4f40
Ken Alex Fassone <nexusventure@gmail.com>
2015-01-29 22:11:41 +0000

    Added array, created a minimal application for rack

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,5 +1,11 @@
 require "rulers/version"
+require "rulers/array"
 
 module Rulers
-  # Your code goes here...
+	class Application
+		def call(env)
+			`echo debug > debug.txt`;
+			[200, {'Content-Type' => 'text/html'},["Hello, world from rulers!"]]
+		end
+	end
 end
